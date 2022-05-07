e0d53e9bc8f68da38d8263177f2a9c57968c4662
HeetAnkola <heet.ankola2019@vitbhopal.ac.in>
2022-05-07 22:11:29 +0530

    itemadded

--- src/Component/ItemType.js
+++ src/Component/ItemType.js
@@ -1,43 +1,130 @@
-import React, { Component } from 'react'
-import Item from './Item'
+import React, { Component } from "react";
+import Item from "./Item";
 
 export class ItemType extends Component {
   render() {
     return (
-      <div className='container my-3'>
+      <div className="container my-3">
         <div className="row">
           <div className="col md-4">
-        <Item title="Xiaomi 11 Lite NE 5G (Jazz Blue 6GB RAM 128 GB Storage)..." link="https://amzn.to/3MH24bZ" imglink="https://m.media-amazon.com/images/I/71XmlboxNZL._SL1500_.jpg"/></div>
-        <div className="col md-4">
-        <Item title="GLUN Bolt Electronic Portable Fishing Hook Type Digital..." link="https://amzn.to/3vZ7Rml" imglink="https://m.media-amazon.com/images/I/41g2KGfKWGL.jpg"/></div>
-        <div className="col md-4">
-        <Item title="Prime Membership" link="https://amzn.to/3KCgicS" imglink="https://m.media-amazon.com/images/G/31/marketing/prime/inline-prime-logo._CB485922431_.png"/>
-        <Item title="Audible Membership" link="https://www.amazon.in/hz/audible/mlp?ie=UTF8&linkCode=ll2&tag=homies03-21&linkId=7641632f10f37af5e737ed932a1a4ee9&language=en_IN&ref_=as_li_ss_tl" imglink="https://images-eu.ssl-images-amazon.com/images/G/31/AudibleIN/2020/AMZ/subnav/audible_logo_subnav_427x100._CB424619030_.jpg"/></div>
-        <div className="col md-4">
-        <Item title="Zero to One" link="https://amzn.to/3LDZkvM" imglink="https://m.media-amazon.com/images/I/41ltkuJcb1L.jpg"/></div>
-        <div className="col md-4">
-        <Item title="The Complete Stories of Sherlock Holmes" link="https://amzn.to/3vBoVAb" imglink="https://m.media-amazon.com/images/I/51zPorhZ19L.jpg"/></div>
-        <div className="col md-4">
-        <Item title="ASUS VivoBook 14 (2021), 14-inch (35.56 cm) FHD, Intel ..." link="https://amzn.to/3vD2iLz" imglink="https://m.media-amazon.com/images/I/71WuDXpTAlL._SL1500_.jpg"/></div>
-        <div className="col md-4">
-        <Item title="MSI Modern 14, Intel i5-10210U, 14 FHD IPS-Level 60Hz ..." link="https://amzn.to/37cH2mw" imglink="https://m.media-amazon.com/images/I/81uEixOREFL._SL1500_.jpg"/></div>
-        <div className="col md-4">
-        <Item title="pTron Bassbuds Duo New Bluetooth 5.1 Wireless Headphone..." link="https://amzn.to/3OUxX2z" imglink="https://m.media-amazon.com/images/I/519uMzMXJVL._SL1100_.jpg"/></div>
-        <div className="col md-4">
-        <Item title="Python: Programming for Advanced: Learn the Fundamental..." link="https://amzn.to/3wpea39" imglink="https://m.media-amazon.com/images/I/41hCp01Ck0L.jpg"/></div>
-        <div className="col md-4">
-        <Item title="PYTHON: PROGRAMMING: A BEGINNERS GUIDE TO LEARN PYTHON..." link="https://amzn.to/39x2cMN" imglink="https://m.media-amazon.com/images/I/41eUu1vIpaL.jpg"/></div>
-        <div className="col md-4">
-        <Item title="Python Programming: Your Advanced Guide To Learn Python..." link="https://amzn.to/3w2ZXbP" imglink="https://m.media-amazon.com/images/I/4191DNJ9ByL.jpg"/></div>
-        <div className="col md-4">
-        <Item title="Blake Pierce: Mystery Bundle (Before He Kills, Cause to..." link="https://amzn.to/37YOF0p" imglink="https://m.media-amazon.com/images/I/41M3Ilc52+L._SX260_.jpg"/></div>
-        <div className="col md-4">
-        <Item title="Casio Enticer Ladies Analog White Dial Women's Watch-A1807" link="https://amzn.to/389LZwQ" imglink="https://m.media-amazon.com/images/I/612Rs5A1chL._UL1440_.jpg"/></div>
-        <div className="col md-4">
-        <Item title="WOW Imagine Shock Proof Clear Protective Back Case for Samsung Galaxy M33 5G - AirEDGE Technology" link="https://amzn.to/3P4MRTV" imglink="https://m.media-amazon.com/images/I/51JWM9OBzbL._SL1000_.jpg"/></div>
-        </div></div>
-    )
+            <Item
+              title="Xiaomi 11 Lite NE 5G (Jazz Blue 6GB RAM 128 GB Storage)..."
+              link="https://amzn.to/3MH24bZ"
+              imglink="https://m.media-amazon.com/images/I/71XmlboxNZL._SL1500_.jpg"
+            />
+          </div>
+          <div className="col md-4">
+            <Item
+              title="GLUN Bolt Electronic Portable Fishing Hook Type Digital..."
+              link="https://amzn.to/3vZ7Rml"
+              imglink="https://m.media-amazon.com/images/I/41g2KGfKWGL.jpg"
+            />
+          </div>
+          <div className="col md-4">
+            <Item
+              title="Prime Membership"
+              link="https://amzn.to/3KCgicS"
+              imglink="https://m.media-amazon.com/images/G/31/marketing/prime/inline-prime-logo._CB485922431_.png"
+            />
+            <Item
+              title="Audible Membership"
+              link="https://www.amazon.in/hz/audible/mlp?ie=UTF8&linkCode=ll2&tag=homies03-21&linkId=7641632f10f37af5e737ed932a1a4ee9&language=en_IN&ref_=as_li_ss_tl"
+              imglink="https://images-eu.ssl-images-amazon.com/images/G/31/AudibleIN/2020/AMZ/subnav/audible_logo_subnav_427x100._CB424619030_.jpg"
+            />
+          </div>
+          <div className="col md-4">
+            <Item
+              title="Zero to One"
+              link="https://amzn.to/3LDZkvM"
+              imglink="https://m.media-amazon.com/images/I/41ltkuJcb1L.jpg"
+            />
+          </div>
+          <div className="col md-4">
+            <Item
+              title="The Complete Stories of Sherlock Holmes"
+              link="https://amzn.to/3vBoVAb"
+              imglink="https://m.media-amazon.com/images/I/51zPorhZ19L.jpg"
+            />
+          </div>
+          <div className="col md-4">
+            <Item
+              title="ASUS VivoBook 14 (2021), 14-inch (35.56 cm) FHD, Intel ..."
+              link="https://amzn.to/3vD2iLz"
+              imglink="https://m.media-amazon.com/images/I/71WuDXpTAlL._SL1500_.jpg"
+            />
+          </div>
+          <div className="col md-4">
+            <Item
+              title="MSI Modern 14, Intel i5-10210U, 14 FHD IPS-Level 60Hz ..."
+              link="https://amzn.to/37cH2mw"
+              imglink="https://m.media-amazon.com/images/I/81uEixOREFL._SL1500_.jpg"
+            />
+          </div>
+          <div className="col md-4">
+            <Item
+              title="pTron Bassbuds Duo New Bluetooth 5.1 Wireless Headphone..."
+              link="https://amzn.to/3OUxX2z"
+              imglink="https://m.media-amazon.com/images/I/519uMzMXJVL._SL1100_.jpg"
+            />
+          </div>
+          <div className="col md-4 my -3">
+            <Item
+              title="Python: Programming for Advanced: Learn the Fundamental..."
+              link="https://amzn.to/3wpea39"
+              imglink="https://m.media-amazon.com/images/I/41hCp01Ck0L.jpg"
+            />
+          </div>
+          <div className="col md-4 my-3">
+            <Item
+              title="PYTHON: PROGRAMMING: A BEGINNERS GUIDE TO LEARN PYTHON..."
+              link="https://amzn.to/39x2cMN"
+              imglink="https://m.media-amazon.com/images/I/41eUu1vIpaL.jpg"
+            />
+          </div>
+          <div className="col md-4 my-3">
+            <Item
+              title="Python Programming: Your Advanced Guide To Learn Python..."
+              link="https://amzn.to/3w2ZXbP"
+              imglink="https://m.media-amazon.com/images/I/4191DNJ9ByL.jpg"
+            />
+          </div>
+          <div className="col md-4 my-3">
+            <Item
+              title="Blake Pierce: Mystery Bundle (Before He Kills, Cause to..."
+              link="https://amzn.to/37YOF0p"
+              imglink="https://m.media-amazon.com/images/I/41M3Ilc52+L._SX260_.jpg"
+            />
+            <Item
+              title="Amazon Prime music"
+              link="https://amzn.to/3N0lPv4"
+              imglink="https://m.media-amazon.com/images/G/31/digital/music/merch/India/2022/Feb/MLP_PC_slice_01.jpg"
+            />
+          </div>
+          <div className="col md-4 my-3">
+            <Item
+              title="Casio Enticer Ladies Analog White Dial Women's Watch-A1807"
+              link="https://amzn.to/389LZwQ"
+              imglink="https://m.media-amazon.com/images/I/612Rs5A1chL._UL1440_.jpg"
+            />
+          </div>
+          <div className="col md-4">
+            <Item
+              title="WOW Imagine Shock Proof Clear Protective Back Case for Samsung Galaxy M33 5G - AirEDGE Technology"
+              link="https://amzn.to/3P4MRTV"
+              imglink="https://m.media-amazon.com/images/I/51JWM9OBzbL._SL1000_.jpg"
+            />
+          </div>
+          <div className="col md-4">
+          <Item
+              title="Bajaj PX 97 Torque New 36L Personal Air Cooler with Honey..."
+              link="https://amzn.to/3MWcSmx"
+              imglink="https://m.media-amazon.com/images/I/61Q6E7laZjL._SL1500_.jpg"
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default ItemType
+export default ItemType;
